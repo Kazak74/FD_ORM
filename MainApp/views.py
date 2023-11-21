243011c620cf6f11b16a7376c0e298825be7327a
Andrey <anich.email@gmail.com>
2023-11-21 12:09:30 +0300

    Initial commit. DONE Practice task 5

---
+++ MainApp/views.py
@@ -0,0 +1,55 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+
+items = [
+   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+   {"id": 7, "name": "Картофель фри" ,"quantity":0},
+   {"id": 8, "name": "Кепка" ,"quantity":124},
+]
+
+
+# Create your views here.
+
+def home(request):
+    text = """<h1>"Изучаем django День первый"</h1>
+            <strong>Автор</strong>: <i>Иванов И.П.</i>"""
+    return HttpResponse(text)
+
+def about(request):
+
+    anketa = {
+        "Имя":"Иван",
+        "Отчество":"Петрович",
+        "Фамилия":"Иванов",
+        "телефон":"8-923-600-01-02",
+        "email":"vasya@mail.ru"
+    }
+    text = f"""Имя: <b>{anketa['Имя']}</b><br>
+        Отчество: <b>{anketa['Отчество']}</b><br>
+        Фамилия: <b>{anketa['Фамилия']}</b><br>
+        телефон: <b>{anketa['телефон']}</b><br>
+        email: <b>{anketa['email']}</b><br>
+"""
+    return HttpResponse(text)
+
+def get_item(requset, id):
+
+    for item in items:
+        if item['id'] == id:
+            text = f"""<h1>Название: {item['name']}</h1> 
+            <p>Количество: {item['quantity']}<p>"""
+            return HttpResponse(text)
+        
+    return HttpResponseNotFound(f"""Товар с {id=} не найден""")
+
+def get_items(requset):
+
+    text = "<h2>Список товаров</h2><ol>"
+    for item in items:
+        text += f"<li>Название: {item['name']}</li>"
+    text += "</ol>" 
+    return HttpResponse(text)
+        
+   # return HttpResponseNotFound(f"""Товар с {id=} не найден""")
