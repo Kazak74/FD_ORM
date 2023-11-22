7306cb88938c6c508156834125ef291e9e1eeea6
Kazak74 <anich.email@gmail.com>
2023-11-22 19:36:07 +0300

    Module 3 ORM2 3

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField() 
+    desc  = models.CharField(max_length=256, default=' ')
 
     def __repr__(self):
         return f"Item(self.name, self.brand, self.count)"
