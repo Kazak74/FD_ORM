820856d0403ec122f3f59a5549b6100c5669afb0
Kazak74 <anich.email@gmail.com>
2023-11-23 11:14:32 +0300

    Add static files svg

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('item/<int:id>', views.get_item, name='item-detail'),
     path('items', views.get_items, name='items-list')
 
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
