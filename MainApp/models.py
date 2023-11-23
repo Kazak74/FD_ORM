324c17c8446394bca52c32cd1a7f8777232a90ad
Kazak74 <anich.email@gmail.com>
2023-11-23 11:58:02 +0300

    Add Color to Item(n:m)

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,12 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+    
+class Color(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __repr__(self):
+        return f"Color({self.name})"
+
+
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField() 
     desc  = models.CharField(max_length=256, default=' ')
+    colors = models.ManyToManyField(to=Color)
 
     def __repr__(self):
         return f"Item(self.name, self.brand, self.count)"
+
 
